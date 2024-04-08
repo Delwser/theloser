ff4eeb05680b719229eeea8cc9052be65f36f86c
Delwser <87214159+Delwser@users.noreply.github.com>
2024-04-08 17:26:36 -0300

    Update g3ntl3m4n.py

--- g3ntl3m4n.py
+++ g3ntl3m4n.py
@@ -3,22 +3,21 @@ import random
 import string
 
 while True:
-    os.makedirs("DarkParasites" "a" , exist_ok=True)
+    os.makedirs("DarkParasites", "a" , exist_ok=True)
     with open(f"DarkParasites/bypass.txt") as arquivo:
          for _ in range(90000):
               arquivo.write(random.choice(string.ascii_letters + string.digits))
 
-    os.makedirs("Th3_g3ntl3m4n" "a" , exist_ok=True)
+    os.makedirs("Th3_g3ntl3m4n", "a" , exist_ok=True)
     with open(f"Th3_g3ntl3m4n/t@nd3r@") as arquivo:
          for _ in range(90000):
               arquivo.write(random.choice(string.ascii_letters + string.digits))
 
 
-    os.makedirs("J0aninh@s" "a" , exist_ok=True)
+    os.makedirs("J0aninh@s", "a" , exist_ok=True)
     with open(f"J0aninh@s/fsociety") as arquivo:
          for _ in range(90000):
               arquivo.write(random.choice(string.ascii_letters + string.digits))
-
 
 
 
